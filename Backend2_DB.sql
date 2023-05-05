84973f689fed40358a46a5600c6b1ed50234cb4e
윤기 <129282769+Yun-Gi@users.noreply.github.com>
2023-05-05 17:31:50 +0900

    Add files via upload

---
+++ Backend2_DB.sql
@@ -0,0 +1,3 @@
+SELECT * FROM compareto.cpu;
+INSERT INTO cpu (Manufacturer, Name, Release_date, Cores, Threads, Manufacturing, Base_Clock, Turbo_Clock,TDP)
+VALUES ('Intel','i7-10700K', '2020-09-00', '8', '16', '14', '2.9', '4.8', '65');
